07b5b35947fa4ec650eb691fd8016fcc9f095382
Leah Anderson <leah@chromebooks.lol>
2026-01-25 11:04:33 -0700

    scrap bitfield setup because i don't feel like making the array calls hypercomplicated

--- include/room.h
+++ include/room.h
@@ -2,20 +2,12 @@
 #define ROOM_H
 #include "dungeon.h"
 #include "things.h"
-// credits to MatzeBraun on stack overflow for this bit that makes it portable to all C compilers
-// url: https://stackoverflow.com/questions/11983231/is-it-safe-to-use-an-enum-in-a-bit-field
-#ifdef __GNUC__ 
-#define ENUMBF(type) __extension__ type
-#else 
-#define ENUMBF(type) unsigned
-#endif
 void changeRoom(Room dungeon[][9], roomPtr target);
 #ifdef __GNUC__
 typedef enum : unsigned {
 #else
 typedef enum {
 #endif
-//  there is a max of 16 possible room types you can use
 	EMPTY,
 	ONEEXIT,
 	TWOEXITS_STRAIGHT,
@@ -23,10 +15,6 @@ typedef enum {
 	THREEEXITS,
 	FOUREXITS
 } ROOMTYPE;
-struct ROOMTYPE_PAIR {
-	ENUMBF(ROOMTYPE) one:4;
-	ENUMBF(ROOMTYPE) two:4;
-};
 extern int room_3exits();
 extern int room_2exits_straight();
 extern int room_2exits_corner();
